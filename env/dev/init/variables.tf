9f8a2fb8b0151c6d8a98ee310e6ed153b5fd033a
ross <ivanov.svc@gmail.com>
2024-03-17 20:51:20 +1300

    Add init config for dev env

---
+++ env/dev/init/variables.tf
@@ -0,0 +1,12 @@
+variable "tags" {
+  type = map(string)
+  default = {
+    env = "dev"
+    owner = "devops"
+  }
+}
+
+variable "terraform_user_id" {
+  type = string
+  default = "AKIA27J4L2ACVE2SLIO4"
+}
