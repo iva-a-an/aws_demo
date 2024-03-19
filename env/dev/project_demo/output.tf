7ac8efa36bd9d424665dbd0743d24442e9b9b235
ross <ivanov.svc@gmail.com>
2024-03-19 15:36:50 +1300

    Add IAM policy to EC2. Update VPC settings

--- env/dev/project_demo/output.tf
+++ env/dev/project_demo/output.tf
@@ -1,3 +1,7 @@
-output "instance_ip" {
-  value = module.demo.instance_ip
+output "vm_a_ip" {
+  value = module.demo.vm_a_ip
+}
+
+output "vm_b_ip" {
+  value = module.demo.vm_b_ip
 }
--- modules/demo/output.tf
+++ modules/demo/output.tf
@@ -1,7 +1,8 @@
-output "instance_ip" {
-  value = aws_instance.demo.public_ip
+output "vm_a_ip" {
+  value = aws_instance.vm_a.public_ip
 }
 
-output "instance_id" {
-  value = aws_instance.demo.id
+output "vm_b_ip" {
+  value = aws_instance.vm_b.public_ip
 }
+
